58e28b7fb0add172137c8c5e8cbd4d3f1b099403
Elenagm-ewewe <egutmor699@g.educaand.es>
2026-05-15 00:31:49 +0200

    config strapi permisos

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -4,13 +4,26 @@ export default [
         name: 'strapi::cors',
         config: {
             enabled: true,
-            origin: ['http://localhost:4321','http://webiesjandula.51.210.104.106.sslip.io'],// tu frontend (Astro)
+            origin: ['http://localhost:4321', 'http://webiesjandula.51.210.104.106.sslip.io'],
             headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
             methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
             credentials: true,
         },
     },
-    'strapi::security',
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'connect-src': ["'self'", 'http:', 'https:'],
+                    'img-src': ["'self'", 'data:', 'blob:', 'strapiwebjandula.51.210.104.106.sslip.io'],
+                    'media-src': ["'self'", 'data:', 'blob:', 'strapiwebjandula.51.210.104.106.sslip.io'],
+                    upgradeInsecureRequests: null, // IMPORTANTE: Esto evita que Strapi intente forzar HTTPS
+                },
+            },
+        },
+    },
     'strapi::poweredBy',
     'strapi::logger',
     'strapi::query',
